1590210f5d3bb318eca63541411095fd6744bd4c
Leonyn <leonyn.rosal@alunoescolassociais.org.br>
2024-09-20 08:23:10 -0300

    modifica o js

---
+++ endereco.js
@@ -0,0 +1,15 @@
+'user strict' //modo restrito
+// este modo faz com que o javaScript operea de formas mais seguras e rigorosa, ajuda a evitar erros comuns de programação
+    /*consumo de API - https://viacep.com.br/ */
+
+
+    const limparforulario = () =>{
+        document.getElementById('lagrodouro').value = '';
+        document.getElementById('bairro').value = '';
+        document.getElementById('localidade').value ='';
+        document.getElementById('uf').value = "";
+    }
+
+   const eNumero = (nuemero) =>{
+    
+   }
