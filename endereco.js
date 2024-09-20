d4e6f975755c61340124a8c0d5e74c099df031ac
Leonyn <leonyn.rosal@alunoescolassociais.org.br>
2024-09-20 09:08:24 -0300

    realiza as alteracoes no repositorio

--- endereco.js
+++ endereco.js
@@ -13,3 +13,13 @@
 // -------------------------------------------------------------------------------------------------------------------------------------------
    const eNumero = (nuemero) => /^[0-9]+$/.teste(numero);
    const cepValido = (cep) => cep.length == 8& eNumero(cep); 
+
+//    função para prencher fromularios com os campos da API
+
+  const preencherFormulario = (endereco) =>{
+      document.getElementById('logradouro').value = endereco.logradouro;
+      document.getElementById('bairro').value = endereco.bairro;
+      document.getElementById('localidade').value = endereco.localidade;
+      document.getElementById('uf').value = endereco.uf;
+
+}
